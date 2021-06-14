ea9ab331a37c3e866369b52786eeede6df704cd6
Pavel <alexanderpavelv32@gmail.com>
2021-06-14 14:43:46 -0600

    Adding alert system with redux

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,17 @@
+import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
+const initialState = [];
+
+const alert = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      console.log(payload);
+      console.log(state.filter((alert) => alert.id !== payload));
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
+  }
+};
+export default alert;
