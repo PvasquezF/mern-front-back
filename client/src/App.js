3c39b3cb8dfab78dd1d31112d990e7613ad1f631
Pavel <alexanderpavelv32@gmail.com>
2021-06-16 07:58:00 -0600

    private route added

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/layout/Dashboard";
 import Alert from "./components/layout/Alert";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -31,6 +33,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register}></Route>
               <Route exact path="/login" component={Login}></Route>
+              <PrivateRoute exact path="/dashboard" component={Dashboard}></PrivateRoute>
             </Switch>
           </section>
         </Fragment>
